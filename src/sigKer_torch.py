1522209c656beb09e0a0ff75a15dfb33d8aae88d
crispitagorico <crispitagorico@gmail.com>
2020-08-27 16:09:49 +0100

    reorganized repo

--- src/sigKer_torch.py
+++ src/sigKer_torch.py
@@ -102,6 +102,51 @@ class SigKernel(torch.autograd.Function):
             return grad_output[:,None,None]*grad_points, None, None
 
 
+# Naive implementation with pytorch auto-diff (slow)
+
+class SigLoss_naive(torch.nn.Module):
+
+    def __init__(self, n=0):
+        super(SigLoss_naive, self).__init__()
+        self.n = n
+
+    def forward(self, X, Y):
+        d = SigKernel_naive(X,X,self.n) + SigKernel_naive(Y,Y,self.n) - 2.*SigKernel_naive(X,Y,self.n)
+        return torch.mean(d)
+
+
+def SigKernel_naive(X,Y,n=0):
+    """
+    input
+     - X a list of A paths each of shape (M,D)
+     - Y a list of A paths each of shape (N,D)
+
+    computes by solving PDEs (forward)
+     -  K_XY: a vector of A pairwise kernel evaluations k(x_i,y_i)      (forward)
+    """
+    A = len(X)
+    M = X[0].shape[0]
+    N = Y[0].shape[0]
+
+    K_XY = torch.zeros((A, (2**n)*(M-1)+1, (2**n)*(N-1)+1)).type(torch.float64)
+    K_XY[:, 0, :] = 1.
+    K_XY[:, :, 0] = 1.
+
+    for i in range(0, (2**n)*(M-1)):
+        for j in range(0, (2**n)*(N-1)):
+
+            ii = int(i / (2 ** n))
+            jj = int(j / (2 ** n))
+
+            inc_X_i = (X[:, ii + 1, :] - X[:, ii, :])/float(2**n)  # (A,D)
+            inc_Y_j = (Y[:, jj + 1, :] - Y[:, jj, :])/float(2**n)  # (A,D)
+
+            increment_XY = torch.einsum('ik,ik->i', inc_X_i, inc_Y_j)  # (A) <-> A dots prod bwn R^D and R^D
+
+            K_XY[:, i + 1, j + 1] = K_XY[:, i + 1, j].clone() + K_XY[:, i, j + 1].clone() + K_XY[:, i,j].clone()* increment_XY.clone() - K_XY[ :, i,j].clone()
+
+    return K_XY[:, -1, -1]
+
 
 def tile(a, dim, n_tile):
     init_dim = a.size(dim)
@@ -110,6 +155,7 @@ def tile(a, dim, n_tile):
     a = a.repeat(*(repeat_idx))
     order_index = torch.LongTensor(np.concatenate([init_dim * np.arange(n_tile) + i for i in range(init_dim)]))
     return torch.index_select(a, dim, order_index)
+
 
 def flip(x, dim):
     xsize = x.size()
--- src/sigMMD_torch.py
+++ src/sigMMD_torch.py
@@ -2,21 +2,23 @@ import numpy as np
 import torch
 from sigKer_fast import sig_kernels_fb_mmd, sig_kernels_f_mmd
 
-class SigLoss_MMD(torch.nn.Module):
+# MMD distance with gradients (fast)
+
+class SigMMD(torch.nn.Module):
 
     def __init__(self, n=0):
-        super(SigLoss_MMD, self).__init__()
+        super(SigMMD, self).__init__()
         self.n = n
 
     def forward(self, X, Y):
-        K_XX = SigKernel_Gram.apply(X,None,self.n)
-        K_YY = SigKernel_Gram.apply(Y,None,self.n)
-        K_XY = SigKernel_Gram.apply(X,Y,self.n)
+        K_XX = SigKerGramMat.apply(X,None,self.n)
+        K_YY = SigKerGramMat.apply(Y,None,self.n)
+        K_XY = SigKerGramMat.apply(X,Y,self.n)
         MMD_squared = torch.mean(K_XX) + torch.mean(K_YY) - 2.*torch.mean(K_XY)
         return MMD_squared
 
 
-class SigKernel_Gram(torch.autograd.Function):
+class SigKerGramMat(torch.autograd.Function):
 
     @staticmethod
     def forward(ctx, X, Y=None, n=0):
@@ -26,7 +28,7 @@ class SigKernel_Gram(torch.autograd.Function):
          - Y a list of B paths each of shape (N,D)
 
         computes by solving PDEs (forward) and by variation of parameter (backward)
-         -  (forward) K_XY: a Gram matrix of pairwise kernel evaluations k(x_i,y_j)   (A,B,M,D)  
+         - (forward) K_XY: a Gram matrix of pairwise kernel evaluations k(x_i,y_j)   (A,B,M,D)  
          - (backward) K_dXY: Gram matrix of gradients ( dk(x_i,y_j)/dx_i )_{i,j} (A,B,M,D)   
         """
         XX, YY, XY = False, False, False
@@ -110,6 +112,56 @@ class SigKernel_Gram(torch.autograd.Function):
             return grad, None, None
 
 
+# Naive implementation of MMD distance with gradients ontain via pytorch automatic differentiation (slow)
+
+class SigMMD_naive(torch.nn.Module):
+
+    def __init__(self, n=0):
+        super(SigMMD_naive, self).__init__()
+        self.n = n
+
+    def forward(self, X, Y):
+        K_XX = SigKerGramMat_naive(X,X,self.n)
+        K_YY = SigKerGramMat_naive(Y,Y,self.n)    
+        K_XY = SigKerGramMat_naive(X,Y,self.n)
+        return torch.mean(K_XX)+torch.mean(K_YY)-2.*torch.mean(K_XY)
+
+
+def SigKerGramMat_naive(X,Y,n=0):
+    """
+    input
+     - X a list of A paths each of shape (M,D)
+     - Y a list of B paths each of shape (N,D)
+
+    computes by solving PDEs (forward)
+     -  K_XY: a gram matrix of pairwise kernel evaluations k(x_i,y_j) 
+    """
+    A = len(X)
+    B = len(Y)
+    M = X[0].shape[0]
+    N = Y[0].shape[0]
+
+    K_XY = torch.zeros((A,B, (2**n)*(M-1)+1, (2**n)*(N-1)+1)).type(torch.float64)
+    K_XY[:,:, 0, :] = 1.
+    K_XY[:,:, :, 0] = 1.
+
+    for i in range(0, (2**n)*(M-1)):
+        for j in range(0, (2**n)*(N-1)):
+
+            ii = int(i / (2 ** n))
+            jj = int(j / (2 ** n))
+
+            inc_X_i = (X[:, ii + 1, :] - X[:,ii, :])/float(2**n)  # (A,D)
+            inc_Y_j = (Y[:, jj + 1, :] - Y[:, jj, :])/float(2**n)  # (B,D)
+
+            increment_XY = torch.einsum('ik,jk->ij', inc_X_i, inc_Y_j)  # (A,B) 
+
+            K_XY[:,:, i + 1, j + 1] = K_XY[:,:, i + 1, j].clone() + K_XY[:,:, i, j + 1].clone() + K_XY[:,:, i,j].clone()* increment_XY.clone() - K_XY[ :,:, i,j].clone()
+
+    return K_XY[:,:, -1, -1]
+
+
+# Various utils functions
 
 def tile(a, dim, n_tile):
     init_dim = a.size(dim)
@@ -118,6 +170,7 @@ def tile(a, dim, n_tile):
     a = a.repeat(*(repeat_idx))
     order_index = torch.LongTensor(np.concatenate([init_dim * np.arange(n_tile) + i for i in range(init_dim)]))
     return torch.index_select(a, dim, order_index)
+
 
 def flip(x, dim):
     xsize = x.size()
