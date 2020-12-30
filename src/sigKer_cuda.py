07d9850fdbeab7877d87027f288133104c283aef
crispitagorico <crispitagorico@gmail.com>
2020-12-30 09:35:22 +0000

    started cuda coding

---
+++ src/sigKer_cuda.py
@@ -0,0 +1,194 @@
+import numpy as np
+import torch
+import torch.cuda
+from numba import jit
+from torch.autograd import Function
+from numba import cuda
+import math
+
+# ----------------------------------------------------------------------------------------------------------------------
+@cuda.jit
+def compute_softdtw_cuda(D, gamma, bandwidth, max_i, max_j, n_passes, R):
+    """
+    :param seq_len: The length of the sequence (both inputs are assumed to be of the same size)
+    :param n_passes: 2 * seq_len - 1 (The number of anti-diagonals)
+    """
+    # Each block processes one pair of examples
+    b = cuda.blockIdx.x
+    # We have as many threads as seq_len, because the most number of threads we need
+    # is equal to the number of elements on the largest anti-diagonal
+    tid = cuda.threadIdx.x
+
+    # Compute I, J, the indices from [0, seq_len)
+
+    # The row index is always the same as tid
+    I = tid
+
+    inv_gamma = 1.0 / gamma
+
+    # Go over each anti-diagonal. Only process threads that fall on the current on the anti-diagonal
+    for p in range(n_passes):
+
+        # The index is actually 'p - tid' but need to force it in-bounds
+        J = max(0, min(p - tid, max_j - 1))
+
+        # For simplicity, we define i, j which start from 1 (offset from I, J)
+        i = I + 1
+        j = J + 1
+
+        # Only compute if element[i, j] is on the current anti-diagonal, and also is within bounds
+        if I + J == p and (I < max_i and J < max_j):
+            # Don't compute if outside bandwidth
+            if not (abs(i - j) > bandwidth > 0):
+                r0 = -R[b, i - 1, j - 1] * inv_gamma
+                r1 = -R[b, i - 1, j] * inv_gamma
+                r2 = -R[b, i, j - 1] * inv_gamma
+                rmax = max(max(r0, r1), r2)
+                rsum = math.exp(r0 - rmax) + math.exp(r1 - rmax) + math.exp(r2 - rmax)
+                softmin = -gamma * (math.log(rsum) + rmax)
+                R[b, i, j] = D[b, i - 1, j - 1] + softmin
+
+        # Wait for other threads in this block
+        cuda.syncthreads()
+
+
+@cuda.jit
+def compute_sigKernel_cuda(M_inc, len_x, len_y, n_anti_diagonals, M_sol):
+    """
+    We start from a list of pairs of paths [(x^1,y^1), ..., (x^n, y^n)]
+    M_inc: a 3-tensor D[i,j,k] = <x^i_j, y^i_k>.
+    n_anti_diagonals = 2 * max(len_x, len_y) - 1
+    M_sol: a 3-tensor storing the solutions of the PDEs.
+    """
+
+    # Each block corresponds to a pair (x_i,y_i).
+    block_id = cuda.blockIdx.x
+    # Each thread works on a node of a diagonal.
+    thread_id = cuda.threadIdx.x
+
+    I = thread_id
+
+    # Go over each anti-diagonal. Only process threads that fall on the current on the anti-diagonal
+    for p in range(n_anti_diagonals):
+
+        # The index is actually 'p - thread_id' but need to force it in-bounds
+        J = max(0, min(p - thread_id, len_y - 1))
+
+        # For simplicity, we define i, j which start from 1 (offset from I, J)
+        i = I + 1
+        j = J + 1
+
+        # Only compute if element[i, j] is on the current anti-diagonal
+        if I + J == p and (I < len_x and J < len_y):
+
+            M_sol[block_id, i, j] = M_sol[block_id, i-1, j] + M_sol[block_id, i, j-1] + M_sol[block_id, i-1, j-1]*(M_inc[block_id, i-1, j-1]-1.)
+
+        # Wait for other threads in this block
+        cuda.syncthreads()
+
+#def forward_step_explicit(double k_00, double k_01, double k_10, double increment):
+#	return (k_10 + k_01)*(1.+0.5*increment+(1./12)*increment**2) - k_00*(1.-(1./12)*increment**2)
+
+#def forward_step_implicit(double k_00, double k_01, double k_10, double increment):
+#	return k_01+k_10-k_00 + ((0.5*increment)/(1.-0.25*increment))*(k_01+k_10)
+
+# ----------------------------------------------------------------------------------------------------------------------
+@cuda.jit
+def compute_softdtw_backward_cuda(D, R, inv_gamma, bandwidth, max_i, max_j, n_passes, E):
+    k = cuda.blockIdx.x
+    tid = cuda.threadIdx.x
+
+    # Indexing logic is the same as above, however, the anti-diagonal needs to
+    # progress backwards
+    I = tid
+
+    for p in range(n_passes):
+        # Reverse the order to make the loop go backward
+        rev_p = n_passes - p - 1
+
+        # convert tid to I, J, then i, j
+        J = max(0, min(rev_p - tid, max_j - 1))
+
+        i = I + 1
+        j = J + 1
+
+        # Only compute if element[i, j] is on the current anti-diagonal, and also is within bounds
+        if I + J == rev_p and (I < max_i and J < max_j):
+
+            if math.isinf(R[k, i, j]):
+                R[k, i, j] = -math.inf
+
+            # Don't compute if outside bandwidth
+            if not (abs(i - j) > bandwidth > 0):
+                a = math.exp((R[k, i + 1, j] - R[k, i, j] - D[k, i + 1, j]) * inv_gamma)
+                b = math.exp((R[k, i, j + 1] - R[k, i, j] - D[k, i, j + 1]) * inv_gamma)
+                c = math.exp((R[k, i + 1, j + 1] - R[k, i, j] - D[k, i + 1, j + 1]) * inv_gamma)
+                E[k, i, j] = E[k, i + 1, j] * a + E[k, i, j + 1] * b + E[k, i + 1, j + 1] * c
+
+        # Wait for other threads in this block
+        cuda.syncthreads()
+
+# We don't need a different backward function for the kernel (see variation of parameters)!!
+# ----------------------------------------------------------------------------------------------------------------------
+
+class _SoftDTWCUDA(Function):
+    """
+    CUDA implementation is inspired by the diagonal one proposed in https://ieeexplore.ieee.org/document/8400444:
+    "Developing a pattern discovery method in time series data and its GPU acceleration"
+    """
+
+    @staticmethod
+    def forward(ctx, D, gamma, bandwidth):
+        dev = D.device
+        dtype = D.dtype
+        gamma = torch.cuda.FloatTensor([gamma])
+        bandwidth = torch.cuda.FloatTensor([bandwidth])
+
+        B = D.shape[0]
+        N = D.shape[1]
+        M = D.shape[2]
+        threads_per_block = max(N, M)
+        n_passes = 2 * threads_per_block - 1
+
+        # Prepare the output array
+        R = torch.ones((B, N + 2, M + 2), device=dev, dtype=dtype) * math.inf
+        R[:, 0, 0] = 0
+
+        # Run the CUDA kernel.
+        # Set CUDA's grid size to be equal to the batch size (every CUDA block processes one sample pair)
+        # Set the CUDA block size to be equal to the length of the longer sequence (equal to the size of the largest diagonal)
+        compute_softdtw_cuda[B, threads_per_block](cuda.as_cuda_array(D.detach()),
+                                                   gamma.item(), bandwidth.item(), N, M, n_passes,
+                                                   cuda.as_cuda_array(R))
+        ctx.save_for_backward(D, R, gamma, bandwidth)
+        return R[:, -2, -2]
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        dev = grad_output.device
+        dtype = grad_output.dtype
+        D, R, gamma, bandwidth = ctx.saved_tensors
+
+        B = D.shape[0]
+        N = D.shape[1]
+        M = D.shape[2]
+        threads_per_block = max(N, M)
+        n_passes = 2 * threads_per_block - 1
+
+        D_ = torch.zeros((B, N + 2, M + 2), dtype=dtype, device=dev)
+        D_[:, 1:N + 1, 1:M + 1] = D
+
+        R[:, :, -1] = -math.inf
+        R[:, -1, :] = -math.inf
+        R[:, -1, -1] = R[:, -2, -2]
+
+        E = torch.zeros((B, N + 2, M + 2), dtype=dtype, device=dev)
+        E[:, -1, -1] = 1
+
+        # Grid and block sizes are set same as done above for the forward() call
+        compute_softdtw_backward_cuda[B, threads_per_block](cuda.as_cuda_array(D_),
+                                                            cuda.as_cuda_array(R),
+                                                            1.0 / gamma.item(), bandwidth.item(), N, M, n_passes,
+                                                            cuda.as_cuda_array(E))
+        E = E[:, 1:N + 1, 1:M + 1]
+        return grad_output.view(-1, 1, 1).expand_as(E) * E, None, None
