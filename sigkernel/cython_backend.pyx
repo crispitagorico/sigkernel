5874799ee91424597864feb45eda753c7bb7c24b
csalvi <crispitagorico@gmail.com>
2022-07-02 14:34:09 +0100

    indentation

--- sigkernel/cython_backend.pyx
+++ sigkernel/cython_backend.pyx
@@ -14,7 +14,6 @@ def sig_kernel_batch_varpar(double[:,:,:] G_static, bint _naive_solver=False):
 	cdef double[:,:,:] K = np.zeros((A,M+1,N+1), dtype=np.float64)
 
 	for l in range(A):
-
 	    for i in range(M+1):
 		    K[l,i,0] = 1.
 
