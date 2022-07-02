53f58c7cc7d0e63597026b1f981727986da8b0d8
csalvi <crispitagorico@gmail.com>
2022-07-02 14:34:43 +0100

    indentation

--- sigkernel/cython_backend.pyx
+++ sigkernel/cython_backend.pyx
@@ -4,34 +4,6 @@
 from libc.math cimport exp
 import numpy as np
 
-def sig_kernel_batch_varpar(double[:,:,:] G_static, bint _naive_solver=False):
-
-	cdef int A = G_static.shape[0]
-	cdef int M = G_static.shape[1]
-	cdef int N = G_static.shape[2]
-	cdef int i, j, l
-
-	cdef double[:,:,:] K = np.zeros((A,M+1,N+1), dtype=np.float64)
-
-	for l in range(A):
-	    for i in range(M+1):
-		    K[l,i,0] = 1.
-
-		for j in range(N+1):
-		    K[l,0,j] = 1.
-
-		for i in range(M):
-			for j in range(N):
-
-			    if _naive_solver:
-			        K[l,i+1,j+1] = (K[l,i+1,j] + K[l,i,j+1])*(1. + 0.5*G_static[l,i,j]) - K[l,i,j]
-			        #K[l,i+1,j+1] = K[l,i+1,j] + K[l,i,j+1] + K[l,i,j]*(G_static[l,i,j] - 1.)
-				else:
-				    K[l,i+1,j+1] = (K[l,i+1,j] + K[l,i,j+1])*(1.+0.5*G_static[l,i,j]+(1./12)*G_static[l,i,j]**2) - K[l,i,j]*(1. - (1./12)*G_static[l,i,j]**2)
-					#K[l,i+1,j+1] = K[l,i+1,j] + K[l,i,j+1] - K[l,i,j] + (exp(0.5*G_static[l,i,j])-1.)*(K[l,i+1,j] + K[l,i,j+1])
-
-	return np.array(K)
-
 
 def sig_kernel_derivative_batch_varpar(double[:,:,:] G_static, double[:,:,:] G_static_direction):
 
