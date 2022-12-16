9b34eac3eb3ea626d8265c3eb4190347ce087ab3
csalvi <crispitagorico@gmail.com>
2022-12-16 07:23:03 +0000

    add signature conditional HSIC

--- sigkernel/sigkernel.py
+++ sigkernel/sigkernel.py
@@ -241,6 +241,7 @@ class SigKernel():
         return K_XX_m + K_YY_m - 2. * torch.mean(K_XY)
 
 
+
 class _SigKernel(torch.autograd.Function):
     """Signature kernel k_sig(x,y) = <S(f(x)),S(f(y))> where k(x,y) = <f(x),f(y)> is a given static kernel"""
  
@@ -648,7 +649,54 @@ def hypothesis_test(y_pred, y_test, static_kernel, confidence_level=0.99, dyadic
 # ===========================================================================================================
 
 
+def SigCHSIC(X, Y, Z, static_kernel, dyadic_order=1, eps=0.1):
+    """Input:
+            - X: torch tensor of shape (batch, length_X, dim_X),
+            - Y: torch tensor of shape (batch, length_Y, dim_Y)
+            - Z: torch tensor of shape (batch, length_Z, dim_Z)
+            - sigma: kernel bandwidth hyperparameter
+            - eps: normalization parameter
 
+        Output: Signature CHSIC
+    """
+
+    device = X.device
+    dtype = X.dtype
+
+    # number of samples
+    m = X.shape[0]
+
+    # centering matrix
+    H = torch.eye(m, dtype=dtype, device=device) - (1. / m) * torch.ones((m, m), dtype=dtype, device=device)
+
+    # initialise signature kernel
+    signature_kernel = SigKernel(static_kernel, dyadic_order)
+
+    # compute signature Gram matrices
+    K_X = signature_kernel.compute_Gram(X, X, sym=True)
+    K_Y = signature_kernel.compute_Gram(Y, Y, sym=True)
+    K_Z = signature_kernel.compute_Gram(Z, Z, sym=True)
+
+    # center Gram matrices
+    K_X_ = H @ K_X @ H
+    K_Y_ = H @ K_Y @ H
+    K_Z_ = H @ K_Z @ H
+
+    # epsilon perturbation of K_Z_
+    K_Z_e = K_Z_ + m * eps * torch.eye(m, device=device)
+
+    # inverting K_Z_e
+    K_Z_e_inv = torch.cholesky_inverse(K_Z_e)
+    K_Z_e_inv2 = K_Z_e_inv @ K_Z_e_inv
+
+    # computing three terms in CHSIC
+    term_1 = torch.trace(K_X_ @ K_Y_)
+    A = K_Z_ @ K_Z_e_inv2 @ K_Z_
+    B = K_X_ @ A @ K_Y_
+    term_2 = torch.trace(B)
+    term_3 = torch.trace(B @ A)
+
+    return (term_1 - 2. * term_2 + term_3) / m ** 2
 
 
 
